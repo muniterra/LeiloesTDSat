8c33493a155f7b857cb7d741baa6cd8be1d2c709
muniterra <shutt.oficial@gmail.com>
2025-03-16 09:29:14 -0300

    Funcionalidade de cadastro (salvar) produto ativa e em funcionamento

--- src/com/leiloestdsat/dao/ProdutosDAO.java
+++ src/com/leiloestdsat/dao/ProdutosDAO.java
@@ -14,6 +14,7 @@ import com.leiloestdsat.dto.ProdutosDTO;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -25,11 +26,37 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        conn = new conectaDAO().connectDB();
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        if (conn != null) {
+            String sql = "INSERT INTO produtos (nome, valor) VALUES (?, ?)";
+
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, produto.getNome());
+                pstmt.setDouble(2, produto.getValor());
+
+                int rowsAffected = pstmt.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    System.out.println("Produto cadastrado com sucesso!");
+                } else {
+                    System.out.println("Falha ao cadastrar o produto.");
+                }
+            } catch (SQLException e) {
+                System.err.println("Erro ao cadastrar produto: " + e.getMessage());
+            } finally {
+                try {
+                    if (conn != null) {
+                        conn.close();
+                    }
+                } catch (SQLException e) {
+                    System.err.println("Erro ao fechar a conexão: " + e.getMessage());
+                }
+            }
+        } else {
+            System.err.println("Falha ao conectar ao banco de dados.");
+        }
+
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
--- src/com/leiloestdsat/dao/conectaDAO.java
+++ src/com/leiloestdsat/dao/conectaDAO.java
@@ -24,7 +24,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?useSSL=false", "root", "root");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
